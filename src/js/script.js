ad4a512964434cbadc605b89a1c1c0e510fc9329
razuvaevcr <razuvaj@gmail.com>
2021-10-27 22:00:57 +0300

    add catalog with tabs and cards

--- src/js/script.js
+++ src/js/script.js
@@ -4,8 +4,29 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         dots: true,
         speed: 1200,
-        adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/prevArrow.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/nextArrow.png"></button>'
     });
+
+    // Catalog поведение карточек товаров
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleClass(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+    
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        })
+    }
+
+    toggleClass('.catalog-item__link');
+    toggleClass('.catalog-item__back');
 });
