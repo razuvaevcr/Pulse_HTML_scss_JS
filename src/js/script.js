9edc6a66248c723648f3ee75814a3b096526db20
razuvaevcr <razuvaj@gmail.com>
2021-11-12 16:18:07 +0300

    validate html, fix animation

--- src/js/script.js
+++ src/js/script.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
                 e.preventDefault();
     
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+                $('.catalog-item__text').eq(i).toggleClass('catalog-item__text_active');
             })
         })
     }
